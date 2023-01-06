562db4e3a98a76616981dd7e8ba800642f733720
BaThanh2303 <thanhnbth2210021@fpt.edu.vn>
2023-01-06 21:19:04 +0700

    idk

--- js/accordion.js
+++ js/accordion.js
@@ -1,7 +1,4 @@
-$('.accordion').on('click', '.accordion-control', function (e){
+$('.accordion').on('click', '.accordion-control', function(e){
     e.preventDefault();
-    $(this)
-        .next('.accordion-panel')
-        .not(':animated')
-        .slideToggle();
+    $(this).next('.accordion-panel').not(':animated').slideToggle();
 });
